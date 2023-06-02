8b60c42214707a7d6ccf4b439769e6f47f7652f7
Andrew William Watson <aww@awwsmm.com>
2023-06-02 12:32:00 -0400

    finished 3.3

--- chapter-03/not-quite-file/src/main.rs
+++ chapter-03/not-quite-file/src/main.rs
@@ -6,6 +6,36 @@ struct File {
     data: Vec<u8>,
 }
 
+impl File {
+    fn new(name: &str) -> File {
+        File {
+            name: String::from(name),
+            data: Vec::new()
+        }
+    }
+
+    fn new_with_data(
+        name: &str,
+        data: &Vec<u8>
+    ) -> File {
+        let mut f = File::new(name);
+        f.data = data.clone();
+        f
+    }
+
+    fn read(
+        self: &File,
+        save_to: &mut Vec<u8>,
+    ) -> usize {
+        let mut tmp = self.data.clone();
+        let read_length = tmp.len();
+        save_to.reserve(read_length);
+        save_to.append(&mut tmp);
+        read_length
+    }
+
+}
+
 fn open(f: &mut File) -> bool {
     true
 }
@@ -14,36 +44,20 @@ fn close(f: &mut File) -> bool {
     true
 }
 
-fn read(
-    f: &File,
-    save_to: &mut Vec<u8>,
-) -> usize {
-    let mut tmp = f.data.clone();
-    let read_length = tmp.len();
-
-    save_to.reserve(read_length);
-    save_to.append(&mut tmp);
-    read_length
-}
-
 fn main() {
-    let mut f2 = File {
-        name: String::from("2.txt"),
-        data: vec![114, 117, 115, 116, 33],
-    };
+    let f3_data = vec![114, 117, 115, 116, 33];
 
-    let mut buffer: Vec<u8> = vec![];
+    let mut f3 = File::new_with_data("2.txt", &f3_data);
 
-    open(&mut f2);
-    let f2_length = read(&f2, &mut buffer);
-    close(&mut f2);
+    let mut buffer = vec![];
+
+    open(&mut f3);
+    let f3_length = f3.read(&mut buffer);
+    close(&mut f3);
 
     let text = String::from_utf8_lossy(&buffer);
 
-    let f2_name = &f2.name;
-    let f2_length = &f2.data.len();
-
-    println!("{:?}", f2);
-    println!("{} is {} bytes long", f2_name, f2_length);
-    println!("{}", text)
+    println!("{:?}", f3);
+    println!("{} is {} bytes long", &f3.name, f3_length);
+    println!("{}", text);
 }
