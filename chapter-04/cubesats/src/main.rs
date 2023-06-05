909e05d42231fc3da9562b9508552dc429862146
Andrew William Watson <aww@awwsmm.com>
2023-06-04 21:48:35 -0400

    completed 4.3

---
+++ chapter-04/cubesats/src/main.rs
@@ -0,0 +1,35 @@
+
+#[derive(Debug)]
+enum StatusMessage {
+    Ok,
+}
+
+#[derive(Debug)]
+struct CubeSat {
+    id: u64,
+}
+
+fn check_status(sat_id: CubeSat) -> StatusMessage {
+    StatusMessage::Ok
+}
+
+fn main() {
+
+    let sat_a = CubeSat { id: 0 };
+    let sat_b = CubeSat { id: 1 };
+    let sat_c = CubeSat { id: 2 };
+
+    let a_status = check_status(sat_a);
+    let b_status = check_status(sat_b);
+    let c_status = check_status(sat_c);
+
+    println!("a: {:?}, b: {:?}, c: {:?}", a_status, b_status, c_status);
+
+    // "waiting"...
+
+    let a_status = check_status(sat_a);
+    let b_status = check_status(sat_b);
+    let c_status = check_status(sat_c);
+
+    println!("a: {:?}, b: {:?}, c: {:?}", a_status, b_status, c_status);
+}
---
+++ chapter-04/notes.md
@@ -0,0 +1,6 @@
+# Chapter 04: Lifetimes, ownership, and borrowing
+
+## 4.1 Implementing a mock CubeSat ground station
+
+"lifetime" vs. "scope" is a difficult distinction for programmers new to Rust.
+
